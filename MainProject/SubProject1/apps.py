3600772ae99e728192ae6dbaa51fda0641e48cf0
thisura97 <tdinith481@gmail.com>
2021-01-09 11:55:19 +0530

    set up django project

---
+++ MainProject/SubProject1/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Subproject1Config(AppConfig):
+    name = 'SubProject1'
---
+++ MainProject/SubProject2/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Subproject2Config(AppConfig):
+    name = 'SubProject2'
